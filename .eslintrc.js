48d39390877dea1c8285ea2b42f83eee5d579bcb
Jacek Tomaszewski <jacek@jtom.me>
2021-02-02 14:56:29 +1100

    Update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,11 @@
-const path = require("path");
-
 module.exports = {
-  extends: [require.resolve("@ailo/eslint-config/configs/ts-node")],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: path.resolve(__dirname, "./tsconfig.json"),
-    // tsconfigRootDir: __dirname,
-    // sourceType: "module",
-    // include: [
-    //   path.resolve(__dirname, "src/**/*.ts"),
-    //   path.resolve(__dirname, "test_modules/**/*.ts")
-    // ]
+    project: ["./tsconfig.json"],
+  },
+  extends: [require.resolve("@ailo/eslint-config/configs/ts-node")],
+  env: {
+    node: true,
+    jest: true,
   },
 };
